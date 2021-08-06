1aa6d382db3453110fcb83f96fda9bff8f02333b
Matthias Konitzny <konitzny@ibr.cs.tu-bs.de>
2021-08-06 11:53:39 +0200

    Fixed imports

--- baselines_lab3/__init__.py
+++ baselines_lab3/__init__.py
@@ -1,2 +1,8 @@
+import os
+import sys
+
+# Allow absolute imports even if project is not installed
+sys.path.insert(0, os.path.dirname(os.getcwd()))
+
 # Import policies to init policy registry
-import baselines_lab3.policies # noqa # pylint: disable=unused-import
+import baselines_lab3.policies  # noqa # pylint: disable=unused-import
