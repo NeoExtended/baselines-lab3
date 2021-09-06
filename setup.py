d662cd2fa278f10de09229262141ea02d017f651
Matthias Konitzny <konitzny@ibr.cs.tu-bs.de>
2021-09-06 11:09:41 +0200

    Added setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="baselines_lab3",
+    packages=[
+        package for package in find_packages() if package.startswith("baselines_lab3")
+    ],
+    version="1.0.0",
+    author="Matthias Konitzny",
+    description="Adds additional environments to the OpenAI Gym package",
+    install_requires=[
+        "gym",
+        "numpy",
+        "stable-baselines3",
+        "optuna",
+        "imageio",
+        "keyboard",
+        "torch>=1.8.1",
+        "pyyaml",
+        "matplotlib",
+    ],
+    include_package_data=True,
+    package_data={"": ["mapdata/*.csv"]},
+)
